733b98d9b0efe5190ead904dc78fae904e61e3d1
Andrew Zhu <mommothazaz123@gmail.com>
2016-10-22 12:34:05 -0700

    Began auto tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneButtonAutoTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by andrew on Oct 22, 2016 as part of ftc_app in org.firstinspires.ftc.teamcode.
+ */
+
+@Autonomous(name="Test: One Beacon", group="NOTDANNY")
+public class OneButtonAutoTest extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+    
+}
+
